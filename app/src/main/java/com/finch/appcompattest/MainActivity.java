9b95b811f55ab60897667070a7f4e7eb56167a06
xt3000 <xt3000@inbox.ru>
2016-12-01 22:48:14 +0500

    Initial commit

---
+++ app/src/main/java/com/finch/appcompattest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.finch.appcompattest;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
